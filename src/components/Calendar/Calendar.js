a9729fd818513c7cec9bcb8e3edb953bc4240793
Enrique Montemayor <eemontemayor@live.com>
2019-04-29 16:57:27 -0500

    added react-calendar

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+
+
+export default class Calendar extends React.Component {
+ 
+
+    
+
+
+
+  render() {
+    
+    return (
+      <div>
+        <h2>Calendar</h2>
+       
+      </div>
+    );
+  }
+}
+
+//each day is a link to add meal page/form which also contains a browser
+// and a view history button that links to components
--- src/components/MealPlannerForm/MealPlannerForm.js
+++ src/components/MealPlannerForm/MealPlannerForm.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
 import { Button, Input } from '../Utils/Utils';
+// import Calendar from '../Calendar/Calendar.js'
+import Calendar from 'react-calendar';
+
 
 export default class MealPlannerForm extends Component{
-    
-    render(){
-        return(
-            <div>
-                <p>
-                    Placeholder for MealPlannerForm
-                    </p>
-            </div>
-        )
-    }
+    state = {
+        date: new Date(),
+      }
+     
+      onChange = date => this.setState({ date })
+   
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
 }
 // renders a calendar component
 // button to adjust calendar length
