a4ecd1bd53ee3651f1135d60f51a0fb2593a43e3
Enrique Montemayor <eemontemayor@live.com>
2019-04-29 10:10:47 -0500

    set up

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+class App extends Component {
+  render(){
+    return (
+      <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+}
+}
+
+export default App;
