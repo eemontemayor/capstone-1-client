6b40e643361345b06dd054e8d1dabb47e435ec1c
Enrique Montemayor <eemontemayor@live.com>
2019-04-29 15:28:54 -0500

    s

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
+
+
+
 export default class HomePage extends Component {
+
+
+    // handleMealBrowserClick -> goes to 3rd party server, etc, 
+
+    // handleMealPlannerClick -> goes to server then database
+
+
     render(){
         return (
             <div>
