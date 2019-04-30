3c619cf543864daedf60498c77d4962335986932
Enrique Montemayor <eemontemayor@live.com>
2019-04-30 09:30:40 -0500

    workin on calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import dateFns from "date-fns";
 import './Calendar.css';
+import {Link} from 'react-router-dom';
 
 class Calendar extends React.Component {
   state = {
@@ -8,7 +9,7 @@ class Calendar extends React.Component {
     selectedDate: new Date()
   };
 
-  renderHeader() {
+  renderHeader() { 
     const dateFormat = "MMMM YYYY";
 
     return (
@@ -28,13 +29,13 @@ class Calendar extends React.Component {
     );
   }
 
-  renderDays() {
+  renderDays() { // renders day names at the top of the col
     const dateFormat = "dddd";
     const days = [];
 
-    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);//Return the start of a week for the given date.
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 7; i++) { //render the jsx for the days in the week
       days.push(
         <div className="col col-center" key={i}>
           {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
@@ -45,13 +46,13 @@ class Calendar extends React.Component {
     return <div className="days row">{days}</div>;
   }
 
-  renderCells() {
+  renderCells() { // renders cells for each day of the week
     const { currentMonth, selectedDate } = this.state;
     const monthStart = dateFns.startOfMonth(currentMonth);
     const monthEnd = dateFns.endOfMonth(monthStart);
-    const startDate = dateFns.startOfWeek(monthStart);
-    const endDate = dateFns.endOfWeek(monthEnd);
-
+    const startDate = dateFns.startOfWeek(monthStart);//show days from the previous month that complete the starting week
+    const endDate = dateFns.endOfWeek(monthEnd);//show days from next month that end the last week of selected month
+                                                  // executed with while loop below
     const dateFormat = "D";
     const rows = [];
 
@@ -62,20 +63,21 @@ class Calendar extends React.Component {
     while (day <= endDate) {
       for (let i = 0; i < 7; i++) {
         formattedDate = dateFns.format(day, dateFormat);
-        const cloneDay = day;
+        const cloneDay = day; 
         days.push(
           <div
             className={`col cell ${
-              !dateFns.isSameMonth(day, monthStart)
+              !dateFns.isSameMonth(day, monthStart) // if it is not the case that this day is in the same month then disable clicks on it
                 ? "disabled"
-                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
-            }`}
+                : dateFns.isSameDay(day, selectedDate) ? "selected" : "" // else if day is the same day as selected Date in our state then add classname selected
+            }`}                                                         // else add an empty string
             key={day}
-            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}// clondDay needed because otherwise onClick will always take endDate as clicked value since that's the value of day when loop ends (because we defined day in outer scope)
           >
             <span className="number">{formattedDate}</span>
             <span className="bg">{formattedDate}</span>
           </div>
+          
         );
         day = dateFns.addDays(day, 1);
       }
@@ -86,10 +88,11 @@ class Calendar extends React.Component {
       );
       days = [];
     }
-    return <div className="body">{rows}</div>;
+    return <div className="body">{rows}</div>; //render the days
   }
 
-  onDateClick = day => {
+  //================= Event handlers for click events===================//
+  onDateClick = day => { 
     this.setState({
       selectedDate: day
     });
@@ -111,8 +114,8 @@ class Calendar extends React.Component {
     return (
       <div className="calendar">
         {this.renderHeader()}
-        {this.renderDays()}
-        {this.renderCells()}
+         {this.renderDays()}
+        {this.renderCells()} 
       </div>
     );
   }
@@ -120,5 +123,7 @@ class Calendar extends React.Component {
 
 export default Calendar;
 
-//each day is a link to add meal page/form which also contains a browser
-// and a view history button that links to components
+//make each day a link to add meal page/form which also contains a browser, history, bookmarks components
+// customize calendar with size adjustor 
+// customize calendar to only show present and future weeks
+// disable prev month function
