8ab4b7a6bf441d7f43cdca12b4515010b813695a
Enrique Montemayor <eemontemayor@live.com>
2019-04-30 17:00:08 -0500

    working on rendering results from browser comp

--- src/components/MealBrowserForm/MealBrowserForm.js
+++ src/components/MealBrowserForm/MealBrowserForm.js
@@ -1,21 +1,99 @@
 import React, {Component} from 'react';
-import { Button, Input } from '../Utils/Utils';
+import { Button, Input, Section } from '../Utils/Utils';
 import MealApiService from '../../services/meal-api-service';
 
 export default class MealBrowserForm extends Component{
-    
+    state={
+        searchTerm:'',
+        searchResuts:[],
+    }
+
+
+    renderResults(...x){
+        
+        
+        let results= x[0].map(i => { 
+            let name = i.recipe.label
+            return `<h4>${name}</h4>`
+          })
+        let htmlString= results.map(i => {
+            return `<div>${i}</div>`
+        })
+        return(
+           
+           console.log(htmlString) 
+         
+        )
+    }
+
 
     handleSubmit = e => {
         e.preventDefault()
-        // const = 
-        // MealApiService.getMeals(sfsdfdf).then...
+        this.setState({
+           
+        })
+        console.log(this.state.searchTerm)
+
+        MealApiService.getMeals(this.state.searchTerm)
+        .then(res =>{
+            
+            this.renderResults(res.hits)
+            
+             
+        })
+        
     }
+    handleChange = (e) => {
+        this.setState({
+          searchTerm: e.target.value
+        });
+      }
     render(){
         return(
             <div>
-                <p>
-                    Placeholder for MealBrowserForm
-                    </p>
+            <form onSubmit={this.handleSubmit}
+            className='BrowserForm'>           
+            <div className='searchTerm'onChange={this.handleChange.bind(this)}>
+              <label htmlFor='addMealForm_search_term'>
+                Search for:
+              </label>
+              <input
+                required
+                type="search"
+                name='searchTerm'
+               
+                id='BrowserForm_search_term'>
+              </input>
+            </div>
+            <div className='dishType'>
+              <label htmlFor='dishType'>MealType
+              </label>
+                <select>
+                    <option value="null">...</option>
+                    <option value="breakfast">Breakfast</option>
+                    <option value="lunch">Lunch</option>
+                    <option value="dinner">Dinner</option>
+                    <option value="dessert">Dessert</option>
+                </select>
+              </div>
+              <div className='cuisineType'>
+              <label htmlFor='cuisineType'>Cuisine Type
+              </label>
+                <Input
+                name="cuisineType"
+                id="BrowserForm_cuisine_search"
+                placeholder="Mexican"
+                >
+                </Input>
+              </div>
+            <Button type='submit'>
+              Search
+            </Button>
+          </form>
+          
+               {/* PLACEHOLDER FOR COMPONENT THAT RENDERS RESULTS */}
+          
+        
             </div>
         )
     }
--- src/services/meal-api-service.js
+++ src/services/meal-api-service.js
@@ -2,11 +2,9 @@ import config from '../config';
 import TokenService from '../services/token-service';
 
 const MealApiService = {
-    getMeals() {
-      return fetch(`${config.API_ENDPOINT}/meals`, { // go to server then go to thirdparty website ?
-        headers: {
-          'authorization':`bearer ${TokenService.getAuthToken()}`,
-        },
+    getMeals(x) {
+      return fetch(`https://api.edamam.com/search?q=${x}&app_id=108438ee&app_key=9fa106c05de3b6d9c71df9aecbab94e6`, { // go to server then go to thirdparty website ?
+       // hide app id and app key when done
       })
         .then(res =>
           (!res.ok)
