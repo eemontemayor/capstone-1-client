22bef6814bfe63a2f3503755a7f2c5faf03f1e37
Enrique Montemayor <eemontemayor@live.com>
2019-04-30 10:42:01 -0500

    added api key to config

--- src/config.js
+++ src/config.js
@@ -2,3 +2,11 @@ export default {
     API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'capstone-client-auth-token',
   }
+
+//   Application ID
+// 108438ee
+// This is the application ID, you should send with each API request.
+
+// Application Keys
+
+// 9fa106c05de3b6d9c71df9aecbab94e6
