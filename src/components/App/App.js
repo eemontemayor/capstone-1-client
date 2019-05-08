df9850da8dfd24e0482b72a5c4d045ad79305a54
Enrique Montemayor <eemontemayor@live.com>
2019-05-08 12:06:21 -0500

    css styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import ApiContext from "../../context/meals-context";
 import config from '../../config';
 import MealApiService from '../../services/meal-api-service';
 import TokenService from '../../services/token-service';
+import './App.css';
 
 
 class App extends Component {
