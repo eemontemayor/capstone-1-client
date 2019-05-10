56e474a31cd8a037dee39ba55d89a4bfc774a864
Enrique Montemayor <eemontemayor@live.com>
2019-05-10 11:21:19 -0500

    commit after deploy

--- src/config.js
+++ src/config.js
@@ -1,12 +1,16 @@
 export default {
     API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'capstone-client-auth-token',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
 
 //   Application ID
 // 108438ee
 // This is the application ID, you should send with each API request.
 
+
+
+//TO-DO remove this if deploy is successful
 // Application Keys
 
 // 9fa106c05de3b6d9c71df9aecbab94e6
