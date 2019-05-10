825b6187a7640a2fd997fcce65867255a05fb9bd
Enrique Montemayor <eemontemayor@live.com>
2019-05-10 13:19:58 -0500

    x

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://hidden-eyrie-68824.herokuapp.com',
+    API_ENDPOINT: 'https://hidden-eyrie-68824.herokuapp.com/',
     TOKEN_KEY: 'capstone-client-auth-token',
     API_KEY: process.env.REACT_APP_API_KEY,
     APP_ID:process.env.REACT_APP_ID,
